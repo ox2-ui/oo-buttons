40cf6640a1b737ac1dbc80d8f3588a8d77c526a7
Lauricio Su <me@lauricio.com>
2015-10-08 21:29:19 +0630

    wip

--- package.js
+++ package.js
@@ -11,11 +11,20 @@ var CS = [C, S];
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
+  // Core
+  api.use([
+    'ecmascript',
+    'session'
+    ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@2.5.0_3'
+    'lauricio:less-autoprefixer@2.5.0_3',
+    'ox2:colors@1.2.0',
+    'ox2:inject-style@1.0.0'
     ]);
   api.addFiles('lib/oo-buttons.less', C);
+  api.addFiles('lib/oo-buttons-theming.js', C);
+
 });
 
 Package.onTest(function(api) {
