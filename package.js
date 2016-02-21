6f523e9ff6d62229712543de30b7f4a73ed66f05
Lauricio Su <me@lauricio.com>
2016-02-21 21:07:51 +0700

    use core less package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:buttons',
-  version: '1.6.0',
+  version: '1.7.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -20,14 +20,14 @@ Package.onUse(function(api) {
   api.use([
     'ecmascript',
     'session',
-    'templating'
+    'templating',
+    'less'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@2.5.0_3',
-    'mquandalle:jade@0.4.1',
+    'mquandalle:jade@0.4.9',
     'ox2:inject-style@1.0.0',
-    'ox2:colors@1.2.0'
+    'ox2:colors@1.7.0'
     ]);
   api.addFiles('lib/oo-buttons.less', C);
   api.addFiles('lib/oo-buttons.jade', C);
