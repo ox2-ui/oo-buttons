17938badd6923dcbcec2e4bc7bdecfe2583a37a2
Lauricio Su <me@lauricio.com>
2016-03-01 19:06:59 +0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:buttons',
-  version: '1.7.0',
+  version: '1.8.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
